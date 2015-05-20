c51848fa19fd514e552fbf5b0a84e172eea9107b
Oscar <Oscar@192.168.0.11>
2015-05-20 22:20:38 +0200

    metodo nombTab()

--- src/projectdatabase/Main.java
+++ src/projectdatabase/Main.java
@@ -17,7 +17,7 @@ public class Main {
     public static void main(String[] args) {
         Conexion.conDerby();
         Consultas query= new Consultas();
-        query.select("alumnos");
+        query.select(Metodos.nombTab());
     }
     
 }
--- src/projectdatabase/Metodos.java
+++ src/projectdatabase/Metodos.java
@@ -5,10 +5,16 @@
  */
 package projectdatabase;
 
+import javax.swing.JOptionPane;
+
 /**
  *
  * @author Oscar
  */
 public class Metodos {
-    
+    public static String nombTab(){
+        String nTab;
+        nTab=JOptionPane.showInputDialog("Introduzca el nombre de la tabla.");
+        return nTab;
+    }
 }
